9587359b7b7379008109db43f000264d6c74072b
Martin Allgood <martinallgood@Martins-MacBook-Air.local>
2018-04-19 16:42:33 +0100

    created game logic

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var cardOne = "King";
+var cardTwo = "King";
+var cardThree = "Queen";
+var cardFour = "Queen";
+
+console.log("User flipped " + cardFour);
+console.log("User flipped " + cardThree);
