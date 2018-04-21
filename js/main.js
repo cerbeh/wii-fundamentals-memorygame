c9add0ab022d98b59c1c7cd61ab1678b52fe470a
Martin Allgood <martinallgood@Martins-MacBook-Air.local>
2018-04-21 20:31:31 +0100

    Module 9 completed

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 var cards = ["King", "King", "Queen", "Queen"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[3];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped ", cardsInPlay);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Better luck next time!")
-	}
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Better luck next time!")
+		}
+	} 
 }
+
+var flipCard =  function (cardId) {
+
+	cardsInPlay.push(cards[cardId]);
+	console.log("User fliped " + cards[cardId]);
+	checkForMatch();
+}
+
+
+flipCard(0);
+flipCard(2);
+
 
